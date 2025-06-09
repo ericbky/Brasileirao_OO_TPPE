fde78bde7a0dbd7f341af4bf0eb8819f74171dea
Eric Silveira <silveirames@hotmail.com>
2025-06-09 19:14:32 -0300

    refactor: ajustando o caminho para as rotas na main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,25 +1,25 @@
 from fastapi import FastAPI
-from app.views.escalacao_routers import router as escalacao_routers
-from app.views.estadio_routers import router as estadio_routers
-from app.views.estatistica_routers import router as estatistica_routers
-from app.views.evento_partida_routers import router as evento_partida_routers
-from app.views.historico_jogador_routers import router as historico_jogador_routers
-from app.views.historico_tecnico_routers import router as historico_tecnico_routers
-from app.views.jogador_routers import router as jogador_routers
-from app.views.partida_routers import router as partida_routers
-from app.views.time_temporada_routers import router as time_temporada_routers
-from app.views.times_routers import router as times_routers
 
+from app.api.escalacao.router import router as escalacao_router
+from app.api.estadio.router import router as estadio_router
+from app.api.estatistica.router import router as estatistica_router
+from app.api.evento_partida.router import router as evento_partida_router
+from app.api.historico_jogador.router import router as historico_jogador_router
+from app.api.historico_tecnico.router import router as historico_tecnico_router
+from app.api.jogador.router import router as jogador_router
+from app.api.partida.router import router as partida_router
+from app.api.time_temporada.router import router as time_temporada_router
+from backend.app.api.time.router import router as times_router
 
 app = FastAPI()
 
-app.include_router(escalacao_routers)
-app.include_router(estadio_routers)
-app.include_router(estatistica_routers)
-app.include_router(evento_partida_routers)
-app.include_router(historico_jogador_routers)
-app.include_router(historico_tecnico_routers)
-app.include_router(jogador_routers)
-app.include_router(partida_routers)
-app.include_router(time_temporada_routers)
-app.include_router(times_routers)
+app.include_router(escalacao_router)
+app.include_router(estadio_router)
+app.include_router(estatistica_router)
+app.include_router(evento_partida_router)
+app.include_router(historico_jogador_router)
+app.include_router(historico_tecnico_router)
+app.include_router(jogador_router)
+app.include_router(partida_router)
+app.include_router(time_temporada_router)
+app.include_router(times_router)
