ab4e594f3e61a35e59ec8b3dee62ddfc9cfd35e9
Eric Silveira <silveirames@hotmail.com>
2025-07-18 22:12:36 -0300

    feat: implementando tabela de jogadores corretamente na pagina de jogadores

--- frontend/src/components/Players/index.tsx
+++ frontend/src/components/Players/index.tsx
@@ -4,9 +4,35 @@ import { useNavigate } from "react-router-dom";
 import search from "./search.png";
 import arrow from "./arrow.png";
 
+interface Jogador {
+    nome: string;
+    idade: number;
+    altura: number;
+    posicao: string;
+    num_camisa: number;
+    convocado_selecao_principal: boolean;
+    convocado_selecao_juniores: boolean;
+    estrangeiro: boolean;
+    valor_mercado: number;
+    time_id: number;
+    id: number;
+}
 
 export const PlayerFrame = () => {
     const navigate = useNavigate();
+    const [jogadores, setJogadores] = React.useState<Jogador[]>([]);
+    const [times, setTimes] = React.useState<{ id: number; nome: string }[]>([]);
+
+    React.useEffect(() => {
+        import("axios").then(axios => {
+            axios.default.get("http://localhost:8001/jogador/listar_jogadores").then((res) => {
+                setJogadores(res.data || []);
+            });
+            axios.default.get("http://localhost:8001/times/listar_times").then((res) => {
+                setTimes(res.data || []);
+            });
+        });
+    }, []);
 
     return (
         <div className="player-frame">
@@ -155,11 +181,11 @@ export const PlayerFrame = () => {
                                     </div>
 
                                     <div className="div-wrapper-7">
-                                        <div className="text-wrapper-6">Time Atual</div>
+                                        <div className="text-wrapper-6">Time</div>
                                     </div>
 
                                     <div className="div-wrapper-8">
-                                        <div className="text-wrapper-6">Gols Marcados</div>
+                                        <div className="text-wrapper-6">Estrangeiro</div>
                                     </div>
 
                                     <div className="div-wrapper-9">
@@ -172,74 +198,41 @@ export const PlayerFrame = () => {
                                 </div>
                             </div>
 
-                            <div className="div-8">
-                                <div className="div-10">
-                                    <div className="div-wrapper-11">
-                                    <div className="text-wrapper-8">Lucas Silva</div>
+                            <div className="div-8" style={{ maxHeight: '400px', overflowY: 'auto', overflowX: 'hidden', minWidth: '100%' }}>
+                                {jogadores.map((jogador) => (
+                                    <div className="div-10" key={jogador.id}>
+                                        <div className="div-wrapper-11">
+                                            <div className="text-wrapper-8">{jogador.nome}</div>
+                                        </div>
+                                        <div className="div-wrapper-12">
+                                            <div className="text-wrapper-9">{jogador.idade}</div>
+                                        </div>
+                                        <div className="div-wrapper-13">
+                                            <div className="text-wrapper-9">{jogador.posicao.charAt(0).toUpperCase() + jogador.posicao.slice(1)}</div>
+                                        </div>
+                                        <div className="div-wrapper-14">
+                                            <div className="text-wrapper-9">{jogador.num_camisa}</div>
+                                        </div>
+                                        <div className="div-wrapper-15">
+                                            <div className="text-wrapper-9">{(times.find(t => t.id === jogador.time_id)?.nome) || jogador.time_id}</div>
+                                        </div>
+                                        <div className="div-wrapper-16">
+                                            <div className="text-wrapper-9">{jogador.estrangeiro ? "Sim" : "Não"}</div>
+                                        </div>
+                                        <div className="div-wrapper-17">
+                                            <div className="text-wrapper-9">R$ {(jogador.valor_mercado).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}M</div>
+                                        </div>
+                                        <div className="div-wrapper-18" style={{ cursor: "pointer" }}>
+                                            <div className="text-wrapper-10" onClick={async () => {
+                                                const axios = (await import('axios')).default;
+                                                await axios.delete(`http://localhost:8001/jogador/deletar_jogador?id=${jogador.id}`);
+                                                window.location.reload();
+                                            }} style={{ color: '#d32f2f', fontWeight: 600 }}>
+                                                Deletar
+                                            </div>
+                                        </div>
                                     </div>
-
-                                    <div className="div-wrapper-12">
-                                    <div className="text-wrapper-9">23</div>
-                                    </div>
-
-                                    <div className="div-wrapper-13">
-                                    <div className="text-wrapper-9">Atacante</div>
-                                    </div>
-
-                                    <div className="div-wrapper-14">
-                                    <div className="text-wrapper-9">9</div>
-                                    </div>
-
-                                    <div className="div-wrapper-15">
-                                    <div className="text-wrapper-9">Time A</div>
-                                    </div>
-
-                                    <div className="div-wrapper-16">
-                                    <div className="text-wrapper-9">12</div>
-                                    </div>
-
-                                    <div className="div-wrapper-17">
-                                    <div className="text-wrapper-9">$1,200,000</div>
-                                    </div>
-
-                                    <div className="div-wrapper-18" onClick={() => navigate("/jogadores/lucas-silva")} style={{ cursor: "pointer" }}>
-                                    <div className="text-wrapper-10">Detalhes</div>
-                                    </div>
-                                </div>
-
-                                <div className="div-10">
-                                    <div className="div-wrapper-11">
-                                    <div className="text-wrapper-8">Bruno Souza</div>
-                                    </div>
-
-                                    <div className="div-wrapper-12">
-                                    <div className="text-wrapper-9">25</div>
-                                    </div>
-
-                                    <div className="div-wrapper-13">
-                                    <div className="text-wrapper-9">Defesa</div>
-                                    </div>
-
-                                    <div className="div-wrapper-14">
-                                    <div className="text-wrapper-9">4</div>
-                                    </div>
-
-                                    <div className="div-wrapper-15">
-                                    <div className="text-wrapper-9">Time B</div>
-                                    </div>
-
-                                    <div className="div-wrapper-16">
-                                    <div className="text-wrapper-9">1</div>
-                                    </div>
-
-                                    <div className="div-wrapper-17">
-                                    <div className="text-wrapper-9">$800,000</div>
-                                    </div>
-
-                                    <div className="div-wrapper-18" onClick={() => navigate("/jogadores/bruno-souza")} style={{ cursor: "pointer" }}>
-                                    <div className="text-wrapper-10">Detalhes</div>
-                                    </div>
-                                </div>
+                                ))}
                             </div>
                         </div>
                     </div>
